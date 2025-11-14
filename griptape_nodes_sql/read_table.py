341b51e411c61705a8b86b7cd5fc40d09c48f47f
Allen Rose <RoseAllenM@gmail.com>
2025-11-13 17:22:19 -0800

    Allow specifying columns to return

--- griptape_nodes_sql/read_table.py
+++ griptape_nodes_sql/read_table.py
@@ -15,10 +15,33 @@ logger = logging.getLogger(__name__)
 
 
 class ReadTable(BaseSQLNode):
-    """Get the full contents of a SQL Table as a list of dictionaries."""
+    """Get the contents of a SQL Table as a list of dictionaries."""
 
     def __init__(self, name: str, metadata: Optional[dict[str, Any]] = None, **kwargs):
         super().__init__(name=name, metadata=metadata, **kwargs)
+
+        self.add_parameter(
+            Parameter(
+                name="headers",
+                input_types=["str"],
+                type="str",
+                output_type="str",
+                tooltip="A comma-separated list of column headers to return.  If not "
+                "supplied, all columns will be returned.",
+            )
+        )
+
+        self.add_parameter(
+            Parameter(
+                name="single_value",
+                input_types=["bool"],
+                type="bool",
+                output_type="bool",
+                default_value=False,
+                tooltip="If true AND a single column is specified, return just a list "
+                "of those values.",
+            )
+        )
 
         self.add_parameter(
             Parameter(
@@ -31,10 +54,28 @@ class ReadTable(BaseSQLNode):
 
     def process(self):
         """Set Node's parameter_output_values and close the SQL connection."""
-        headers = [column[1] for column in self.execute("PRAGMA table_info({table})")]
+        single = False
 
-        self.parameter_output_values["output"] = [
-            dict(zip(headers, row)) for row in self.execute("SELECT * FROM {table}")
-        ]
+        headers = self.parameter_values.get("headers", "").split(",")
+        headers = [header.strip() for header in headers if header]
+
+        if headers and headers != ["*"]:
+            select = ", ".join(headers)
+            if len(headers) == 1:
+                single = self.parameter_values["single_value"]
+        else:
+            headers = [
+                column[1] for column in self.execute("PRAGMA table_info({table})")
+            ]
+            select = "*"
+
+        rows = self.execute(f"SELECT {select} FROM {{table}}")
+
+        if single:
+            self.parameter_output_values["output"] = [row[0] for row in rows]
+        else:
+            self.parameter_output_values["output"] = [
+                dict(zip(headers, row)) for row in rows
+            ]
 
         super().process()
